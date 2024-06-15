a18af866958e77cf6341433ef7f61d9b33753102
Suryadeep Singh <suryadeep.singh098@gmail.com>
2024-06-15 18:00:49 +1000

    set up database interaction

--- essa-backend/server.js
+++ essa-backend/server.js
@@ -1,17 +1,42 @@
 const express = require('express');
+const {connectToDB, getDb} = require('./db');
 
 // init app & middleware:
 
 const app = express()
 
-app.listen(3001, () => {
-    console.log('listening on port 3001');
-} )
+let db;
+
+// db connection:
+connectToDB((err) => {
+    if(!err){
+        app.listen(3001, () => {
+            console.log('listening on port 3001');
+        } )
+
+        db = getDb()
+    }
+    
+})
+
+
 
 // routes:
 
 app.get('/articles', (req, res) => {
-    res.json({mssg: 'hello'})
+    let articles = [];
+
+    db.collection('articles')
+        .find()
+        .sort({author:1})
+        .forEach(article => articles.push(article))
+        .then(() => {
+            res.status(200).json(articles); 
+        })
+        .catch(() => {
+            res.status(500).json({error: 'shit'});
+        });
+
 }
 
 )
